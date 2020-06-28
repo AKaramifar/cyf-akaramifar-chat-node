57567221c1fc982b7f40dd9ac5c911cf3c75a486
Afshin <47743758+AKaramifar@users.noreply.github.com>
2020-06-28 20:55:55 +0100

    update 0.003

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 // Import Library . . .
 const express = require('express');
+let contacts = require('./contacts.json');
 const app = express();
+const fs = require('fs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { json } = require('body-parser');
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -16,6 +19,11 @@ app.get('/',(req, res) => {
         Company: "Code Your Future",
         URL: "https://codeyourfuture.io/"
     })
+})
+// Post New Contact . . .
+app.post('/newcontact', (req, res) => {
+    const newContact = req.body;
+    fs.writeFile(contacts, json.stringify(newContact));
 })
 
 //Listen to port 3000 or any available
