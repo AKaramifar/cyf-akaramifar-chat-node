7e6a402f42e72ff0a1eca8a913fecd63967bcc09
Afshin <47743758+AKaramifar@users.noreply.github.com>
2020-06-28 20:31:54 +0100

    update 0.002

--- server.js
+++ server.js
@@ -17,3 +17,9 @@ app.get('/',(req, res) => {
         URL: "https://codeyourfuture.io/"
     })
 })
+
+//Listen to port 3000 or any available
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+})
