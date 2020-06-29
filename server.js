0c9b73116747dee29586b100782e6f08f5b060c2
Afshin <47743758+AKaramifar@users.noreply.github.com>
2020-06-29 16:50:05 +0100

    update

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 // Import Library . . .
 const express = require("express");
-let contacts = require("./contacts.json");
-let messages = require("./messages.json");
 const app = express();
 const fs = require("fs");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { json } = require("body-parser");
-const { connect } = require("http2");
 
 app.use(cors());
 app.use(bodyParser.json());
+// Read Json File Function . . .
+const readJsonFile = jsonFileURL => {
+  return fs.readFileSync(jsonFileURL, "utf8", err => {
+    console.log("Read File Error: ", err);
+  });
+};
+// Write Json File Function . . .
+const writeJsonFile = (jsonFileURL, newJsonFile) => {
+  fs.writeFileSync(jsonFileURL, JSON.stringify(newJsonFile), "utf8", err => {
+    console.log("Write File Error: ", err);
+  });
+};
 //---------------------- GET ----------------------
 // Main URL: https://cyf-akaramifar-chat-node.herokuapp.com/
 app.get("/", (req, res) => {
@@ -19,70 +27,71 @@ app.get("/", (req, res) => {
     Student_FullName: "Afshin Karamifar",
     Position: "Trainee at Code Your Future",
     Company: "Code Your Future",
-    URL: "https://codeyourfuture.io/",
+    URL: "https://codeyourfuture.io/"
   });
 });
 // Contacts . . .
 app.get("/contacts", (req, res) => {
-  res.json(contacts);
+  let allContacts = JSON.parse(readJsonFile("./contacts.json"));
+  res.json(allContacts);
 });
 // Messages . . .
 app.get("/messages", (req, res) => {
-  res.json(messages);
+  let allMessages = JSON.parse(readJsonFile("./messages.json"));
+  res.json(allMessages);
 });
 //---------------------- POST ----------------------
 // Post New Contact . . .
 app.post("/newcontact", (req, res) => {
-  const newContact = [...contacts, req.body];
-  fs.writeFile("./contacts.json", JSON.stringify(newContact), "utf8", (ERR) =>
-    console.log(ERR)
-  );
+  let newContacts = JSON.parse(readJsonFile("./contacts.json"));
+  newContacts.push(req.body);
+  writeJsonFile("./contacts.json", newContacts);
   res.send("New Contact Added Successfuly!!!");
 });
 // Post New Message . . .
 app.post("/newmessage", (req, res) => {
-  const newMessage = [...messages, req.body];
-  fs.writeFile("./messages.json", JSON.stringify(newMessage), "utf8", (ERR) =>
-    console.log(ERR)
-  );
-  res.send("New Message Adde Successfuly!!!");
+  let newMessages = JSON.parse(readJsonFile("./messages.json"));
+  newMessages.push(req.body);
+  writeJsonFile("./messages.json", newMessages);
+  res.send("New Message Added Successfuly!!!");
 });
-//---------------------- UPDATE ----------------------
+// //---------------------- UPDATE ----------------------
 // Update Contact . . .
-app.put("/updatecontact", (req, res) => {
-  const updatedContact = contacts.map((contact) =>
-    contact.id === req.body.id ? (contact = req.body) : contact
+app.post("/updatecontact", (req, res) => {
+  let allContacts = JSON.parse(readJsonFile("./contacts.json"));
+  let updatedContact = allContacts.map(contact =>
+    contact.contactId === req.body.contactId ? (contact = req.body) : contact
   );
-  fs.writeFile("./contacts.json", JSON.stringify(updatedContact), "utf8", (ERR) =>
-    console.log(ERR)
-  );
-  res.json(contacts);
+  writeJsonFile("./contacts.json", updatedContact);
   res.send("Contact updated Successfuly!!!");
 });
 // Update Message . . .
 app.post("/updatemessage", (req, res) => {
-  const updatedMessage = [...messages, req.body];
-  fs.writeFile("./messages.json", JSON.stringify(newMessage), "utf8", (ERR) =>
-    console.log(ERR)
+  let allMessages = JSON.parse(readJsonFile("./messages.json"));
+  let updatedMessages = allMessages.map(message =>
+    message.messageId === req.body.messageId ? (message = req.body) : message
   );
-  res.send("New Message Adde Successfuly!!!");
+  writeJsonFile("./messages.json", updatedMessages);
+  res.send("Message Updated Successfuly!!!");
 });
 //---------------------- DELETE ----------------------
 // Delete Contact . . .
-app.post("/newcontact", (req, res) => {
-  const newContact = [...contacts, req.body];
-  fs.writeFile("./contacts.json", JSON.stringify(newContact), "utf8", (ERR) =>
-    console.log(ERR)
+app.delete("/deletecontact/:contactid", (req, res) => {
+  let allContacts = JSON.parse(readJsonFile("./contacts.json"));
+  let updatedContact = allContacts.filter(
+    contact => contact.contactId !== req.params.contactid
   );
-  res.send("New Contact Added Successfuly!!!");
+  writeJsonFile("./contacts.json", updatedContact);
+  res.send("Contact Deleted Successfuly!!!");
 });
 // Delete Message . . .
-app.post("/newmessage", (req, res) => {
-  const newMessage = [...messages, req.body];
-  fs.writeFile("./messages.json", JSON.stringify(newMessage), "utf8", (ERR) =>
-    console.log(ERR)
+app.delete("/deletemessage/:messageid", (req, res) => {
+  let allMessages = JSON.parse(readJsonFile("./messages.json"));
+  let updatedMessages = allMessages.filter(
+    message => message.messageId !== req.params.messageid
   );
-  res.send("New Message Adde Successfuly!!!");
+  writeJsonFile("./messages.json", updatedMessages);
+  res.send("Message Deleted Successfuly!!!");
 });
 //Listen to port 3000 or any available
 const PORT = process.env.PORT || 3000;
